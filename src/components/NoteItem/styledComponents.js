2e9697b4601b1e9e90ff199bd2e01cc3733f11c2
Bharath <129857904+bharath-kadiyala@users.noreply.github.com>
2023-07-03 12:21:27 +0530

    Update styledComponents

--- src/components/NoteItem/styledComponents.js
+++ src/components/NoteItem/styledComponents.js
@@ -1,23 +1,44 @@
 import styled from 'styled-components'
 
 export const ListItem = styled.li`
+  //   display: flex;
+  //   flex-direction: column;
+  //   justify-content: center;
+  //   padding: 10px;
+  //   box-shadow: 2px 2px 6px #cbd5e1;
+  //   border-radius: 8px;
+  //   margin-right: 20px;
+  //   margin-bottom: 20px;
+
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 10px;
-  box-shadow: 2px 2px 6px #cbd5e1;
+  max-width: 20vw;
+  height: 25vh;
+  border: 1px solid #cbd5e1;
   border-radius: 8px;
+  margin-right : 16px
+  margin-bottom: 16px;
+  padding: 24px 16px;
 `
 export const CardTitle = styled.h1`
-  font-family: 'Roboto';
-  font-size: 12px;
-  color: #1e293b;
-  margin-bottom: 8px;
-  font-weight: 600;
+  //   font-family: 'Roboto';
+  //   font-size: 12px;
+  //   color: #1e293b;
+  //   margin-bottom: 8px;
+  //   font-weight: 600;
+
+  font-size: 15px;
+  line-height: 0.5;
 `
 export const CardText = styled.p`
+  //   font-family: 'Roboto';
+  //   font-size: 12px;
+  //   color: #1e293b;
+
+  color: #334155;
   font-family: 'Roboto';
-  font-size: 12px;
-  color: #1e293b;
+  font-size: 14px;
+  margin: 0;
+  margin-top: 16px;
+  line-height: 1.33;
 `
